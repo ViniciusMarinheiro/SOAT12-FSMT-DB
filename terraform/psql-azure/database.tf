12ab18bcb22d217bc707f502958a6175e66a7689
ViniciusMarinheiro <viniciusmrsilva@outlook.com>
2026-01-07 23:40:41 -0300

    fix: ajusta database agora vai

--- terraform/psql-azure/database.tf
+++ terraform/psql-azure/database.tf
@@ -5,23 +5,23 @@ resource "random_password" "pg_password" {
 }
 
 resource "azurerm_postgresql_flexible_server" "pg_server" {
-  name = "psql-oficina-fsmt-soat-12" 
-  
+  name = "psql-oficina-fsmt-soat-12"
+
   resource_group_name = data.azurerm_resource_group.rg.name
-  
+
   # CORREÇÃO: Localização real do seu banco
-  location            = "westus" 
-  
+  location = "westus"
+
   # CORREÇÃO: Versão real do seu banco
-  version             = "15" 
-  
-  administrator_login    = "postgres_admin" 
+  version = "15"
+
+  administrator_login    = "postgres_admin"
   administrator_password = random_password.pg_password.result
-  
-  sku_name   = "B_Standard_B1ms" 
+
+  sku_name   = "B_Standard_B1ms"
   storage_mb = 32768
-  
-  public_network_access_enabled = true 
+
+  public_network_access_enabled = true
 
   tags = {
     Environment = "Development"
@@ -30,8 +30,8 @@ resource "azurerm_postgresql_flexible_server" "pg_server" {
 }
 
 resource "azurerm_postgresql_flexible_server_database" "pg_db" {
-  name      = "oficina_db" 
+  name      = "oficina_db"
   server_id = azurerm_postgresql_flexible_server.pg_server.id
   charset   = "UTF8"
-  collation = "en_US.utf8" 
+  collation = "en_US.utf8"
 }
