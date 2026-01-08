64be6577be4fc2db9798d7bf033e2cad67c518a9
ViniciusMarinheiro <viniciusmrsilva@outlook.com>
2026-01-07 23:38:00 -0300

    fix: ajusta database.tf e remove binarios grandes

--- terraform/psql-azure/database.tf
+++ terraform/psql-azure/database.tf
@@ -5,24 +5,23 @@ resource "random_password" "pg_password" {
 }
 
 resource "azurerm_postgresql_flexible_server" "pg_server" {
-  # CORRIGIDO: Nome exato que está na sua connection string
-  name = "psql-oficina-fsmt-soat-12"
-
+  name = "psql-oficina-fsmt-soat-12" 
+  
   resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
-
-  version = "14" # Confirme se criou como versão 14 ou 16
-
-  # CORRIGIDO: Usuário exato da connection string
-  administrator_login    = "postgres_admin"
+  
+  # CORREÇÃO: Localização real do seu banco
+  location            = "westus" 
+  
+  # CORREÇÃO: Versão real do seu banco
+  version             = "15" 
+  
+  administrator_login    = "postgres_admin" 
   administrator_password = random_password.pg_password.result
-
-  # Ajuste o SKU se o seu manual for diferente (ex: B_Standard_B1ms é o mais barato)
-  sku_name   = "B_Standard_B1ms"
+  
+  sku_name   = "B_Standard_B1ms" 
   storage_mb = 32768
-
-  # IMPORTANTE: Se você acessa de fora da Azure, isso deve ser true
-  public_network_access_enabled = true
+  
+  public_network_access_enabled = true 
 
   tags = {
     Environment = "Development"
@@ -30,11 +29,9 @@ resource "azurerm_postgresql_flexible_server" "pg_server" {
   }
 }
 
-# Este bloco cria um banco DENTRO do servidor. 
-# A connection string usa 'postgres' (padrão), mas é ideal ter um para a app.
 resource "azurerm_postgresql_flexible_server_database" "pg_db" {
-  name      = "oficina_db" # Sugiro não usar 'meu_banco...', use um nome real
+  name      = "oficina_db" 
   server_id = azurerm_postgresql_flexible_server.pg_server.id
   charset   = "UTF8"
-  collation = "en_US.utf8"
+  collation = "en_US.utf8" 
 }
